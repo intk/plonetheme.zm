a4aa6c3f02107f328b9a8e3fcc89e4259eb3d44c
André Gonçalves <andreslb1@gmail.com>
2015-04-22 14:10:19 +0200

    Initial commit

---
+++ plonetheme/zm/browser/interfaces.py
@@ -0,0 +1,41 @@
+from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
+from zope.viewlet.interfaces import IViewletManager
+from plone.portlets.interfaces import IPortletManager
+from plone.app.portlets.interfaces import IColumn
+
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+"""
+Base interfaces for further development and to adapt and extend theme
+for particular websites
+"""
+
+class IBrowserLayer(IDefaultBrowserLayer):
+    """plone.app.event specific browser layer.
+    """
+
+class IMediaTypes(Interface):
+    """
+    Marks all Media types from Products.media*
+    """
+
+class IThemeSpecific(Interface):
+    """Marker interface that defines a Zope 3 browser layer.
+    """
+
+class IFooterPortlet(IColumn):
+    """we need our own portlet manager for the footer.
+    """
+
+class IPagePortlet(IColumn):
+    """we need our own portlet manager for the footer.
+    """
+
+class ICollectionPortletManager(IColumn):
+    """we need our own portlet manager for the footer.
+    """
+
+class ISearchPortletManager(IColumn):
+    """we need our own portlet manager for the footer.
+    """
